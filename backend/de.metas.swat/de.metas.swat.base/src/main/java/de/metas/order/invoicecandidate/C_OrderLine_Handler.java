6f5eefe8c5340ee732c0230f524c37fa0477661a
Adrian <adrian.stefan@metasfresh.com>
2024-07-19 11:35:32 +0300

    DO NOT MERGE demo fix

--- backend/de.metas.swat/de.metas.swat.base/src/main/java/de/metas/order/invoicecandidate/C_OrderLine_Handler.java
+++ backend/de.metas.swat/de.metas.swat.base/src/main/java/de/metas/order/invoicecandidate/C_OrderLine_Handler.java
@@ -367,10 +367,11 @@ public class C_OrderLine_Handler extends AbstractInvoiceCandidateHandler
 			@NonNull final I_C_Invoice_Candidate ic,
 			@NonNull final org.compiere.model.I_C_OrderLine orderLine)
 	{
-		if (!ic.isSOTrx())
-		{
-			return;
-		}
+		//FIXME commented for demo, please don't integrate
+		// if (!ic.isSOTrx())
+		// {
+		// 	return;
+		// }
 
 		final PaymentTermId paymentTermId = Services.get(IOrderLineBL.class).getPaymentTermId(orderLine);
 		ic.setC_PaymentTerm_ID(paymentTermId.getRepoId());
